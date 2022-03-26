d6fa96fc2ce0c29d2d576c2734ec869d7ad93619
admin <admin@DESKTOP-OQ11OCH>
2022-03-26 15:34:59 +0530

    commit

--- FileHandling/src/com/jspider/filehandling/example/FolderCreateExample.java
+++ FileHandling/src/com/jspider/filehandling/example/FolderCreateExample.java
@@ -1,6 +1,21 @@
 package com.jspider.filehandling.example;
 
+import java.io.File;
 
 public class FolderCreateExample {
+	
+	public static void main(String[] args) {
+		
+		 File f = new File("C:\\Users\\admin\\Desktop\\WEJM3");
+		 if (f.exists()==true) {
+			// exist() :  check wthere folder is present or not
+			 System.out.println("Folder Already created ");
+		}
+		 else
+		 {
+			 f.mkdir();
+			 System.out.println("folder successfully created ");
+		 }
+	}
 
 }
