ff28868b147cc0e587cb79ee573a5a07f116be98
admin <admin@DESKTOP-OQ11OCH>
2022-03-26 15:41:02 +0530

    commit

---
+++ FileHandling/src/com/jspider/filehandling/example/FileCreationExample.java
@@ -0,0 +1,23 @@
+package com.jspider.filehandling.example;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileCreationExample {
+	public static void main(String[] args) {
+		String path = "C:\\Users\\admin\\Desktop\\WEJM3\\hemendra.txt";
+		File file = new File(path);
+		if (file.exists()==true) {
+			System.out.println("file already created ");
+		} else {
+			try {
+				file.createNewFile();
+				System.out.println("file already created");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
